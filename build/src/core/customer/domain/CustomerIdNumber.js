6e2c5a676e3e1c8ed3010791e9b280b02744b52a
Sebastian Hernandez <sebassium@Sebastians-MacBook-Pro-2.local>
2020-09-26 20:10:42 -0500

    Exclude test folder in build.

--- build/src/core/customer/domain/CustomerIdNumber.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CustomerIdNumber = void 0;
-const ValueObject_1 = require("../../shared/domain/ValueObject");
-class CustomerIdNumber extends ValueObject_1.ValueObject {
-}
-exports.CustomerIdNumber = CustomerIdNumber;
